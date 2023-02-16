fa2f8193bb5584299ea101d297c7107f9fded5c6
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-02-17 01:27:59 +0800

    Interface function for creating cells

--- modified.py
+++ modified.py
@@ -22,3 +22,22 @@ def __init__(self): #initializing game window and organizing tkinter widgets int
 
     #running the tkinter event loop
     self.mainloop()
+
+def interface(self):
+    # making grids in array structure
+    self.cells = []
+    for i in range(4):
+        row = []
+        for j in range(4):
+            cell_frame = tk.Frame(
+                self.main_grid,
+                bg = s.cellColor,
+                width = 130,
+                height = 130
+            )
+            cell_frame.grid(row=i, column=j, padx=5, pady=5)
+            cell_number = tk.Label(self.main_grid, bg=s.cellColor)
+            cell_number.grid(row=i, column=j)
+            cell_data = {"frame": cell_frame, "number": cell_number}
+            row.append(cell_data)
+        self.cells.append(row)
