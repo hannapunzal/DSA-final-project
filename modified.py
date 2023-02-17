701c8bf4958e0f943c90bd9b8a081b8d6e39ad5e
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2023-02-17 11:07:15 +0800

    Game function for filling in a random cell with 2 after every move

--- modified.py
+++ modified.py
@@ -41,3 +41,28 @@ def interface(self):
             cell_data = {"frame": cell_frame, "number": cell_number}
             row.append(cell_data)
         self.cells.append(row)
+
+def initGame(self):
+    #matrix of 0
+    self.matrix = [[0] * 4 for _ in range(4)]
+
+    #filling grids with 2s in random positions
+    row = random.randint(0, 3)
+    col = random.randint(0, 3)
+    self.matrix[row][col] = 2
+    self.cells[row][col]["frame"].configure(bg=s.cellColors[2])
+    self.cells[row][col]["number"].configure(
+    bg=s.cellColors[2],
+    fg=s.cellNumberColors[2],
+    font=s.numberFonts[2],
+    text="2")
+    while(self.matrix[row][col] != 0):
+        row = random.randint(0, 3)
+        col = random.randint(0, 3)
+    self.matrix[row][col] = 2
+    self.cells[row][col]["frame"].configure(bg=s.cellColors[2])
+    self.cells[row][col]["number"].configure(
+        bg=s.cellColors[2],
+        fg=s.cellNumberColors[2],
+        font=s.numberFonts[2],
+        text="2")
